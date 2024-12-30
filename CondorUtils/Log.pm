1ada5b494e7ada40705bafbbc68145c8f3f6a89d
Deji Adegbite <d2alphame@yahoo.com>
2024-12-30 01:06:45 +0100

    Added first commit 9f CondorUtils/Log.pm

---
+++ CondorUtils/Log.pm
@@ -0,0 +1,30 @@
+package CondorUtils::Log;
+
+use v5.34;
+
+sub create($);
+sub info($$);
+
+# The logging levels I am using
+# 1. FATAL
+# 2. ERROR
+# 3. WARN
+# 4. INFO
+# 5. DEBUG
+# Use 0 to ignore all logging
+my $CONFIGURED_LEVEL = 5;                # Set this to 0 to disable all logging
+
+my $LEVEL_FATAL = 1;
+my $LEVEL_ERROR = 2;
+my $LEVEL_WARN = 3;
+my $LEVEL_INFO = 4;
+my $LEVEL_DEBUG = 5;
+
+sub info {
+    return unless $CONFIGURED_LEVEL;                # Return if Logging is turned off
+    return if $CONFIGURED_LEVEL > $LEVEL_INFO;      # Don't log above 'info'. So fatal, error, warn and info will be logged but not debug
+    
+
+}
+
+1;
