af14e4ee5a7f3fe76b7a1d7189f52d6ee0295408
Deji Adegbite <d2alphame@yahoo.com>
2024-12-30 18:20:45 +0100

    Logging work in progress

--- condorUtils/Logger.pm
+++ condorUtils/Logger.pm
@@ -14,23 +14,41 @@ sub info($);
 # Use 0 to ignore all logging
 # Anything greater than 5 will log all
 my $CONFIGURED_LEVEL = 3;                # Set this to 0 to disable all logging
-
 my $LEVEL_FATAL = 1;
 my $LEVEL_ERROR = 2;
 my $LEVEL_WARN = 3;
 my $LEVEL_INFO = 4;
 my $LEVEL_DEBUG = 5;
 
+my $TARGETS;                            # Reference to an array of file handles.
+
+
+my sub hub {
+    return unless $CONFIGURED_LEVEL;    # If Configured Level is 0, then logging is switched off
+    my $log_details = $_[0]->();
+}
+
+
+sub debug($) {
+    return unless $CONFIGURED_LEVEL;
+
+}
+
 sub info($) {
-    return unless $CONFIGURED_LEVEL;                # Return if Logging is turned off
-    return if $CONFIGURED_LEVEL > $LEVEL_INFO;      # Don't log above 'info'. So fatal, error, warn and info will be logged but not debug
+    my ($self, $message) = @_;
     my $thread_id = threads->tid();
-    
+    threads->create(\&hub, $self, $message, $LEVEL_INFO, $thread_id)->detach;
 }
 
 sub create($) {
     my $class = shift;
-    return bless {}, $class;
+    my %params = @_;
+
+    return bless sub {
+        return $params{\%params}
+    }, $class;
 }
+
+
 
 1;
