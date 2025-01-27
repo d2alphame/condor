bffb5018a04d0da94d2d30ebb5065747c86bbd15
Deji Adegbite <d2alphame@yahoo.com>
2025-01-27 06:08:47 +0100

    Processing configs

--- condorUtils/CondorLogging.pm
+++ condorUtils/CondorLogging.pm
@@ -1,15 +1,47 @@
 package CondorUtils::CondorLogging;
 
 use v5.34;
+use strict;
+use warnings;
+
 use Carp;
 
-my $IS_CONFIGURED = 0;
+# The possible logging levels. If you have a shiny new level to log with, add it here
+my @LEVELS = qw(
+    fatal
+    error
+    warn
+    info
+    debug
+);
+my %LEVELSH = map { $LEVELS[$_] => $_ + 1 } 0..$#LEVELS;
+
+my $IS_CONFIGURED       = '';
+my $CONFIGURED_LEVEL    = $LEVELS[-1];          # Default to the highest level
+my $AGGREGATE_ONLY      = 1;                    # Aggregate ALL logs by default
+my @HANDLES;                                    # The file handles to log to
 
 
+# This is used to configure loggers.
 my sub ConfigureLoggers {
-    return if $IS_CONFIGURED;               # Only configure once
+    return if $IS_CONFIGURED;                   # Only configure once
+    my $caller_package = caller;    
+    shift if $caller_package eq $_[0];          # Desppite our best efforts, this still got called with the arrow notation *facepalm*
+    return unless @_;                           # The user provided no configs.
 
-    
+    # Do sanity checks 
+    my @configs = @_;
+    if(defined $configs{aggregate_only}) { $AGGREGATE_ONLY = $configs{aggregate_only} }
+    if(defined $configs{level}) {
+        if(exists $LEVELSH{$configs{level}}) {
+            say "Invalid logging level: $configs{level}. Using default level $CONFIGURED_LEVEL";
+        }
+        else { $CONFIGURED_LEVEL = $configs{level} }
+    }
+
+
+
+
 
     $IS_CONFIGURED = 1;
 }
